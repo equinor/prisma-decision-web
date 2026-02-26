3767142345cf3d9b985462926651affde3ec7e04
rupakkatwal <rupak2072@gmail.com>
2026-02-26 08:24:13 +0100

    export feature only in dev and test

--- src/components/Homepage/HomePage.tsx
+++ src/components/Homepage/HomePage.tsx
@@ -8,6 +8,7 @@ import { useGetEdges } from '../../hooks/api/useGetEdges';
 import { useGetInfluenceNodes } from '../../hooks/api/useGetInfluenceNodes';
 import { CreateProject } from '../common/ProjectInformation/CreateProject';
 import { ImportProject } from '../common/ProjectInformation/ImportProject';
+import { isDev, isTest } from '../../utils/getEnvironment';
 
 export const HomePage = () => {
 	const { projects } = useGetProjects();
@@ -43,7 +44,7 @@ export const HomePage = () => {
 						/>
 						<div className='flex justify-between gap-2'>
 							<CreateProject />
-							<ImportProject />
+							{(isDev() || isTest()) && <ImportProject />}
 						</div>
 					</div>
 					<div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5'>
