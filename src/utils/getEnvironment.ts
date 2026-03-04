03e8d6d9cff0603584d72cec1b7cd0dd35b09d4c
FredrikMWold <fredrik.wold@bouvet.no>
2026-03-04 13:18:54 +0100

    fix: isDev helper function now works as expected

--- src/utils/getEnvironment.ts
+++ src/utils/getEnvironment.ts
@@ -1,3 +1,3 @@
-export const isDev = () => import.meta.env.DEV;
+export const isDev = () => import.meta.env.MODE === 'dev';
 export const isProd = () => import.meta.env.PROD;
 export const isTest = () => import.meta.env.MODE === 'test';
