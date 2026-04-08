01e5476ce58e74657b90afbbe081cd6bda3cb11f
rupakkatwal <rupak2072@gmail.com>
2026-04-08 14:36:55 +0200

    fix prettier formatting

--- src/components/common/DeleteIssueDialog.tsx
+++ src/components/common/DeleteIssueDialog.tsx
@@ -10,9 +10,7 @@ export const DeleteIssueDialog = ({ issue, open = false, onClose }: DeleteIssueD
 			data-no-dnd
 			className='nodrag nopan nowheel pointer-events-auto fixed top-1/2
 			left-1/2 -translate-x-1/2 -translate-y-1/2 transform cursor-auto'
-
-
->
+		>
 			<DialogContent>
 				<div className='flex flex-col gap-4 text-center'>
 					<h2 className='text-2xl font-semibold'>Delete Issue</h2>
