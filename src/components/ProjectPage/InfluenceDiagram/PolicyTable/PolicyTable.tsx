01f95f9d8f924132d9be2bf82000bc98b90aea16
rupakkatwal <rupak2072@gmail.com>
2026-08-19 10:27:04 +0200

    refactored

--- src/components/ProjectPage/InfluenceDiagram/PolicyTable/PolicyTable.tsx
+++ src/components/ProjectPage/InfluenceDiagram/PolicyTable/PolicyTable.tsx
@@ -1,5 +1,5 @@
 import { useMemo } from 'react';
-import { Button, Icon, StarProgress } from '@equinor/eds-core-react';
+import { Button, Icon } from '@equinor/eds-core-react';
 import { close } from '@equinor/eds-icons';
 import { Issue } from '../../../../validators';
 import { CardContainer } from '../../../common/Cards/CardContainer';
@@ -8,25 +8,11 @@ import { usePolicyTable } from './usePolicyTable';
 import { cn } from '../../../../utils/cn';
 
 export const PolicyTable = ({ issue, selected, onClose, ref }: PolicyTableProps) => {
-	const { isFetching, optionIds, parents, parentRowSpans, rows, lookups } = usePolicyTable(issue);
+	const { optionIds, parents, parentRowSpans, rows, lookups } = usePolicyTable(issue);
 	const optionsById = useMemo(
 		() => new Map(issue.decision.options.map(option => [option.id, option] as const)),
 		[issue.decision.options],
 	);
-
-	if (isFetching) {
-		return (
-			<div
-				ref={ref}
-				className='border-background-medium bg-background-default w-87.5 rounded-sm border border-dashed px-3 py-2'
-			>
-				<div className='flex min-h-16 items-center justify-center'>
-					<StarProgress size={24} />
-				</div>
-			</div>
-		);
-	}
-
 	if (!rows.length) {
 		return (
 			<div
