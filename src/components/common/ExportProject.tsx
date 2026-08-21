0927f0d55e19ca148251f4493df5e0629a5c7f93
rupakkatwal <rupak2072@gmail.com>
2026-08-21 14:39:17 +0200

    fix objective export

--- src/components/common/ExportProject.tsx
+++ src/components/common/ExportProject.tsx
@@ -2,15 +2,25 @@ import { Button, Icon, Tooltip } from '@equinor/eds-core-react';
 import { download } from '@equinor/eds-icons';
 import { useGetEdges } from '../../hooks/api/useGetEdges';
 import { useGetIssues } from '../../hooks/api/useGetIssues';
-import { Edge, Issue, Project } from '../../validators';
+import { useGetObjectives } from '../../hooks/api/useGetObjectives';
+import { Edge, Issue, Objective, Project } from '../../validators';
 
 export const ExportProject = ({ project, showLabel }: DownloadProjectJsonButtonProps) => {
 	const { issues } = useGetIssues();
 	const { edges } = useGetEdges();
+	const { objectives } = useGetObjectives();
 	const projectIssues: Issue[] = issues.filter(issue => issue.project_id === project.id);
 	const projectEdges = edges.filter(edge => edge.project_id === project.id);
+	const projectObjectives: Objective[] = objectives.filter(
+		objective => objective.project_id === project.id,
+	);
 
-	const convertToJson = (data: { projects: Project; issues: Issue[]; edges: Edge[] }) => {
+	const convertToJson = (data: {
+		projects: Project;
+		Objectives: Objective[];
+		issues: Issue[];
+		edges: Edge[];
+	}) => {
 		const json = JSON.stringify(data, null, 2);
 		const blob = new Blob([json], { type: 'application/json' });
 		const url = URL.createObjectURL(blob);
@@ -31,6 +41,7 @@ export const ExportProject = ({ project, showLabel }: DownloadProjectJsonButtonP
 
 					convertToJson({
 						projects: project,
+						Objectives: projectObjectives,
 						issues: projectIssues,
 						edges: projectEdges,
 					});
