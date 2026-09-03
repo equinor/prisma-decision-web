dafb86ce358805e3dff148a5c7139557d7dacef4
Fredrik Mikal Wold <fredrik.wold@bouvet.no>
2026-09-03 09:34:31 +0200

    fix: navigate user to new project on duplicate (#753)

--- src/hooks/api/useDuplicateProject.ts
+++ src/hooks/api/useDuplicateProject.ts
@@ -2,23 +2,26 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { apiClient } from '../../api';
 import { Project } from '../../validators';
 import { showErrorToast, showSuccessToast } from '../../components/ShowToast';
+import { useNavigate } from 'react-router';
 
 export const useDuplicateProject = () => {
 	const queryClient = useQueryClient();
+	const navigate = useNavigate();
 	return useMutation({
 		mutationFn: async (id: string): Promise<Project> => {
 			const response = await apiClient.post(`/projects/${id}/duplicate`);
 			return response.data;
 		},
-		onSuccess: () => {
+		onSuccess: async data => {
 			showSuccessToast('Project duplicated successfully');
-			queryClient.refetchQueries({ queryKey: ['projects'] });
 			queryClient.refetchQueries({ queryKey: ['issues'] });
 			queryClient.refetchQueries({ queryKey: ['nodes'] });
 			queryClient.refetchQueries({ queryKey: ['edges'] });
 			queryClient.refetchQueries({ queryKey: ['objectives'] });
 			queryClient.refetchQueries({ queryKey: ['probabilityTables'] });
 			queryClient.refetchQueries({ queryKey: ['utilityTables'] });
+			await queryClient.refetchQueries({ queryKey: ['projects'] });
+			navigate(`/project/${data.id}`);
 		},
 		onError: () => {
 			showErrorToast('Failed to duplicate project');
